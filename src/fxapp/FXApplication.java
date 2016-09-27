8bceeed84c6e0d98f272170d8edbbbdb0745f263
Kavish Patel <Kpatel6987@users.noreply.github.com>
2016-09-27 14:40:00 -0400

    Update FXApplication.java

--- src/fxapp/FXApplication.java
+++ src/fxapp/FXApplication.java
@@ -1,31 +1,31 @@
 package fxapp;
 
-        import controller.CourseOverviewController;
-        import controller.MainScreenController;
-        import controller.StudentEditController;
+//        import controller.CourseOverviewController;
+//        import controller.MainScreenController;
+//        import controller.StudentEditController;
 
-        import javafx.application.Application;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Scene;
-        import javafx.scene.layout.AnchorPane;
-        import javafx.scene.layout.BorderPane;
-        import javafx.stage.Modality;
-        import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-        import model.Student;
+//import model.User;
 
-        import java.io.IOException;
-        import java.util.logging.Level;
-        import java.util.logging.Logger;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
- * Main application class.  Some code reused from the code makery tutorial
+ * Main application class.
  *
  * This class handles all the scene switching to reuse the main stage.
  *
  */
-public class MainFXApplication extends Application {
+public class FXApplication extends Application {
     /**  the java logger for this class */
     private static final Logger LOGGER =Logger.getLogger("MainFXApplication");
 
@@ -58,7 +58,7 @@ public class MainFXApplication extends Application {
         try {
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainFXApplication.class.getResource("../view/MainScreen.fxml"));
+            loader.setLocation(FXApplication.class.getResource("../view/MainScreen.fxml"));
             rootLayout = loader.load();
 
             // Give the controller access to the main app.
@@ -66,7 +66,7 @@ public class MainFXApplication extends Application {
 //            controller.setMainApp(this);
 
             // Set the Main App title
-            mainScreen.setTitle("Main Pagex");
+            mainScreen.setTitle("Main Page");
 
             // Show the scene containing the root layout.
             Scene scene = new Scene(rootLayout);
@@ -95,7 +95,7 @@ public class MainFXApplication extends Application {
         try {
             // Load course overview.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainFXApplication.class.getResource("../view/loginPage.fxml"));
+            loader.setLocation(FXApplication.class.getResource("../view/loginPage.fxml"));
             AnchorPane loginPage = loader.load();
 
             // Set login page into the center of root layout.
@@ -113,46 +113,37 @@ public class MainFXApplication extends Application {
 
     }
 
-    /**
-     * Opens a dialog to edit details for the specified student. If the user
-     * clicks OK, the changes are saved into the provided person object and true
-     * is returned.
-     *
-     * We can also open the dialog to add a completely new student if we pass null in
-     *
-     * @param student the person object to be edited, or null for a new student
-     * @return true if the user clicked OK, false otherwise.
-     */
-    public boolean showStudentAddDialog(Student student) {
-        try {
-            // Load the fxml file and create a new stage for the popup dialog.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainFXApplication.class.getResource("../view/StudentEditDialog.fxml"));
-            AnchorPane page = loader.load();
 
-            // Create the dialog Stage.
-            Stage dialogStage = new Stage();
-            dialogStage.setTitle("Edit Student");
-            dialogStage.initModality(Modality.WINDOW_MODAL);
-            dialogStage.initOwner(mainScreen);
-            Scene scene = new Scene(page);
-            dialogStage.setScene(scene);
-
-            // Set the person into the controller.
-            StudentEditController controller = loader.getController();
-            controller.setDialogStage(dialogStage);
-            controller.setStudent(student);
-
-            // Show the dialog and wait until the user closes it
-            dialogStage.showAndWait();
-
-            return controller.isOkClicked();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+//    public boolean showStudentAddDialog(Student student) {
+//        try {
+//            // Load the fxml file and create a new stage for the popup dialog.
+//            FXMLLoader loader = new FXMLLoader();
+//            loader.setLocation(MainFXApplication.class.getResource("../view/StudentEditDialog.fxml"));
+//            AnchorPane page = loader.load();
+//
+//            // Create the dialog Stage.
+//            Stage dialogStage = new Stage();
+//            dialogStage.setTitle("Edit Student");
+//            dialogStage.initModality(Modality.WINDOW_MODAL);
+//            dialogStage.initOwner(mainScreen);
+//            Scene scene = new Scene(page);
+//            dialogStage.setScene(scene);
+//
+//            // Set the person into the controller.
+//            StudentEditController controller = loader.getController();
+//            controller.setDialogStage(dialogStage);
+//            controller.setStudent(student);
+//
+//            // Show the dialog and wait until the user closes it
+//            dialogStage.showAndWait();
+//
+//            return controller.isOkClicked();
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//    }
 
 
     public static void main(String[] args) {

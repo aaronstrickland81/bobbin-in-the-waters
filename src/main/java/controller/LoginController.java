44269ec251d855c54431f6f1addeded4a132a0bf
Kavish Patel <kpatel6987@gatech.edu>
2016-10-12 12:52:51 -0400

    Databased login verification

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -70,8 +70,7 @@ public class LoginController {
     private void handleLoginAttempt() {
 
         if (isInputValid()) {
-            //FXApplication.setUser(UserDatabaseInterface.verifyUser(userField
-            //   .getText(), pwField.getText()));
+
             FXApplication.setUser(MySQLdb.verifyUser(userField
                     .getText(), pwField.getText()));
             String errorMessage = "";
@@ -81,8 +80,8 @@ public class LoginController {
                 // Show the error message if bad data
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.initOwner(_dialogStage);
-                alert.setTitle("Incorrect password and/or username ");
-                alert.setHeaderText("Please correct invalid fields");
+                alert.setTitle("Please Correct Invalid Fields ");
+                alert.setHeaderText("Incorrect password and/or username");
                 alert.setContentText(errorMessage);
 
                 alert.showAndWait();

e1e59ac4ee59bb9f4e59ad6b24bdf98a67c06223
Kavish Patel <kpatel6987@gatech.edu>
2016-10-16 19:03:34 -0400

    Reports Table Controller Skeleton

---
+++ src/main/java/controller/ReportsTableController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import fxapp.FXApplication;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import model.Model;
+
+/**
+ * Created by kavish on 10/16/16.
+ */
+public class ReportsTableController {
+
+    @FXML
+    private TableView tableview;
+
+    @FXML
+    private TableColumn tableColumn;
+
+    /**
+     * The window for this dialog
+     */
+    private Stage _dialogStage;
+
+    // REf to FX APP
+    private FXApplication app;
+
+    private Model model = Model.getInstance();
+
+    
+}

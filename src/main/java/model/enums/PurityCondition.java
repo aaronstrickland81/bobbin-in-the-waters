333f631d18d5c4c2d56b8eb86973544055476ff3
Kavish Patel <kpatel6987@gatech.edu>
2016-10-27 18:44:02 -0400

    Added database support for Water Quality Reports and finished enum

--- src/main/java/model/enums/PurityCondition.java
+++ src/main/java/model/enums/PurityCondition.java
@@ -4,7 +4,26 @@ package model.enums;
  * Created by Neil on 10/11/2016.
  */
 public enum PurityCondition {
-    Safe,
-    Treatable,
-    Unsafe;
+    SAFE("Safe"),
+    TREATABLE("Treatable"),
+    UNSAFE("Unsafe");
+
+    String cond;
+
+    PurityCondition(String str) {
+        this.cond = str;
+    }
+
+    public String toString() {
+        return cond;
+    }
+
+    public static PurityCondition getCondition(String tString) {
+        for (PurityCondition t : PurityCondition.values()) {
+            if (t.toString().equals(tString)) {
+                return t;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/services/QualityReportInfo.java
+++ src/main/java/services/QualityReportInfo.java
@@ -1,7 +1,8 @@
 package services;
 
-import model.Report;
+import model.WaterQualityReport;
 import model.WaterSourceReport;
+import model.enums.PurityCondition;
 import model.enums.SourceCondition;
 import model.enums.WaterType;
 
@@ -9,33 +10,29 @@ import java.sql.*;
 import java.util.ArrayList;
 
 /**
- * Created by kavish on 10/16/16.
+ * Created by kavish on 10/27/16.
  */
-public class SourceReportInfo {
-
-    /**
-     * Add a new report to the database
-     *
-     * @param report The report to add
-     */
-    public static void addSourceReport(WaterSourceReport report) {
+public class QualityReportInfo {
+    
+    public static void addQualityReport(WaterQualityReport report) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection(
                     "jdbc:mysql://bobbindb.chwrjcnilfzs.us-west-2.rds" +
                             ".amazonaws.com:3306/bobbin", "root", "password");
-            String query = " insert into sourceReportInfo (date, " +
-                    "reportNumber, " +
-                    "username, longitude, latitude, type, cond) "
-                    + "        values  (?, ?, ?, ?, ?, ?, ?)";
+            String query = " insert into qualityReportInfo (date, " +
+                    "reportNumber, username, longitude, latitude, cond, " +
+                    "virus, chem) "
+                    + "        values  (?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement ps = con.prepareStatement(query);
             ps.setDate(1, (Date) report.get_date());
             ps.setInt(2, report.get_reportNumber());
             ps.setString(3, report.get_workername());
             ps.setDouble(4, report.get_longitude());
             ps.setDouble(5, report.get_latitude());
-            ps.setString(6, report.get_type().toString());
-            ps.setString(7, report.get_condition().toString());
+            ps.setString(6, report.get_condition().toString());
+            ps.setDouble(7, report.get_virusPPM());
+            ps.setDouble(8, report.get_chemPPM());
             con.close();
         } catch (Exception e) {
             System.out.println(e);
@@ -45,21 +42,21 @@ public class SourceReportInfo {
     /**
      * @return
      */
-    public static ArrayList<WaterSourceReport> getSourceReports() {
-        ArrayList<WaterSourceReport> aList = new ArrayList<>();
+    public static ArrayList<WaterQualityReport> getQualityReports() {
+        ArrayList<WaterQualityReport> aList = new ArrayList<>();
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection(
                     "jdbc:mysql://bobbindb.chwrjcnilfzs.us-west-2.rds" +
                             ".amazonaws.com:3306/bobbin", "root", "password");
             PreparedStatement ps = con.prepareStatement("select * from " +
-                    "sourceReportInfo");
+                    "qualityReportInfo");
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                WaterSourceReport w = new WaterSourceReport(rs.getDate(1), rs
+                WaterQualityReport w = new WaterQualityReport(rs.getDate(1), rs
                         .getInt(2), rs.getString(3), rs.getDouble(4), rs
-                        .getDouble(5), WaterType.getType(rs.getString(6)),
-                        SourceCondition.getCondition(rs.getString(7)));
+                        .getDouble(5), PurityCondition.getCondition(rs
+                        .getString(6)), rs.getDouble(7), rs.getDouble(8));
                 aList.add(w);
             }
             con.close();
@@ -71,3 +68,4 @@ public class SourceReportInfo {
     }
 
 }
+

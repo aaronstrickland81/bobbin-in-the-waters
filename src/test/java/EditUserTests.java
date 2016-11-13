a99ba312693f1080853d03c3c09d9a77bce45862
Aaron Strickland <astrickland30@gatech.edu>
2016-11-13 16:45:33 -0500

    EditUserTests added, not very comprehensive at the moment

--- src/test/java/EditUserTests.java
+++ src/test/java/EditUserTests.java
@@ -1,3 +1,13 @@
+import model.Model;
+import model.User;
+import model.enums.AccountType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import services.UserInfoTable;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * JUnit test cases for the editUser method
  *
@@ -5,5 +15,59 @@
  */
 public class EditUserTests {
 
+    private User testUser;
 
+    @Before
+    public void init() {
+        testUser = new User("user123", "pass", AccountType.USER,
+                "example@test.com", "fName", "lName");
+        Model.addUser(testUser);
+    }
+
+    /**
+     * Executed after every test. Asserts that the user was properly edited in
+     * the database and then cleans up (removes the user from the database).
+     */
+    @After
+    public void checkEqual() {
+        Model.editUser(testUser);
+        assertEquals(testUser, UserInfoTable.getUserFromUserName(
+                testUser.getUname()));
+        //TODO: remove user
+    }
+
+    @Test
+    public void editPass() {
+        testUser.setPassword("newPass");
+    }
+
+    @Test
+    public void editAccountType() {
+        testUser.setType(AccountType.WORKER);
+    }
+
+    @Test
+    public void editEmail() {
+        testUser.setEmail("newExample@test.com");
+    }
+
+    @Test
+    public void editFirstName() {
+        testUser.setFname("newFName");
+    }
+
+    @Test
+    public void editLastName() {
+        testUser.setLname("newLName");
+    }
+
+    @Test
+    public void editAddress() {
+        testUser.setHomeAddress("1234 Example Lane");
+    }
+
+    @Test
+    public void editTitle() {
+        testUser.setTitle("Mr.");
+    }
 }
